68597208b4f4d103e49c4520528b6c65e3095da6
Hossein Douzandeh <douzandeh.k@gmail.com>
2026-06-30 16:49:34 +0330

    feat: import Contact.jsx => App.jsx - Added: src/components/Contact.jsx => for Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
+import Hero from "./components/Hero";
 import Technologies from "./components/Technologies";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Hero />
         <Technologies />
         <Projects />
+        <Contact />
       </div>
     </div>
   );
